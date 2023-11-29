ad5baf02a530369a457dcc33f62953d7d55ca476
Hamidreza Bahramian <hamid.bahramian@gmail.com>
2023-11-28 19:46:05 -0500

    .

--- app/screens/Login.tsx
+++ app/screens/Login.tsx
@@ -1,16 +1,77 @@
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  ActivityIndicator,
+  Button,
+} from "react-native";
 import React, { useState } from "react";
 import { FIREBASE_AUTH } from "../../FirebaseConfig";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
   const auth = FIREBASE_AUTH;
 
+  const signIn = async () => {
+    setLoading(true);
+    try {
+      const response = await signInWithEmailAndPassword(auth, email, password);
+      console.log(response);
+    } catch (error: any) {
+      console.log(error);
+      alert("Sign in failed: " + error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const signUp = async () => {
+    setLoading(true);
+    try {
+      const response = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      alert("Check your emails!");
+    } catch (error: any) {
+      console.log(error);
+      alert("Registration failed: " + error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <TextInput style={styles.input}/>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry={true}
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+      />
+      {loading ? (
+        <ActivityIndicator size="large" />
+      ) : (
+        <>
+          <Button style={styles.button} title="Login" onPress={signIn} />
+          <Button style={styles.button} title="Create account" onPress={signUp} />
+        </>
+      )}
     </View>
   );
 };
@@ -18,17 +79,21 @@ const Login = () => {
 export default Login;
 
 const styles = StyleSheet.create({
-   container: {
+  container: {
     marginHorizontal: 20,
     flex: 1,
-    justifyContent: 'center'
-   },
-   input: {
+    justifyContent: "center",
+  },
+  input: {
     marginVertical: 4,
     height: 50,
     borderWidth: 1,
     borderRadius: 4,
     padding: 10,
-    backgroundColor: '#fff'
-   }
+    backgroundColor: "#fff",
+  },
+  button: {  
+    margin: 10, 
+    padding: 10    
+  },
 });
